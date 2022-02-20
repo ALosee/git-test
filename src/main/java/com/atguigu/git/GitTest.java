40ed811e62186a4afcdb50ff023cb239784b86dd
jsy <jsy@qq.com>
2022-02-20 13:22:58 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author jiang
+ * @date 2022/2/20 13:19
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
